1602d7e5b7db94180864f1f1d4b559784a2296cd
arjun <a.p.arjun1103@gmail.com>
2025-10-08 12:28:35 +0530

    Create Constructor Demo.java

---
+++ oops-special-concepts/Constructor Demo/Constructor Demo.java
@@ -0,0 +1,9 @@
+public class ConstructorDemo {
+    ConstructorDemo() {
+        System.out.println("Constructor executed automatically when object is created.");
+    }
+
+    public static void main(String[] args) {
+        new ConstructorDemo();
+    }
+}
